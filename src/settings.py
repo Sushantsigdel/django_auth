3137b312836233d2bad62a341710941f4a62c607
Sushant Sigdel <susi_csit2077@lict.edu.np>
2024-09-20 19:40:12 +0545

    Refactor JWT authentication to use custom implementation

--- src/settings.py
+++ src/settings.py
@@ -161,7 +161,7 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'src.apps.auth.auth.CustomJWTAuthentication',
     ],
 }
 
@@ -173,3 +173,6 @@ DJOSER = {
     'PASSWORD_RESET_CONFIRM_RETYPE': True,
     'TOKEN_MODEL': None,
 }
+
+# Auth Cookie settings
+AUTH_COOKIE_NAME = 'access_token'
